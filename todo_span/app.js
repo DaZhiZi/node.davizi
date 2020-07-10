4f59e0f6844f86637191e15162e78ac0db4feedc
Li Chao <1836671897@qq.com>
2020-07-10 11:14:44 +0800

    todospan 还剩 更新没有完成

---
+++ todo_span/app.js
@@ -0,0 +1,132 @@
+const fs = require('fs')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const {log} = require('./utils')
+
+const app = express()
+
+const todoList = []
+
+
+app.use(express.static('static_files'))
+app.use(bodyParser.json())
+
+const sendHtml = (response, path) => {
+    let options = {
+        encoding: 'utf-8',
+    }
+    fs.readFile(path, options, (error, data) => {
+        log(`读取的 html 文件 ${path} 内容是`, data)
+        response.send(data)
+    })
+}
+
+app.get('/', (request, response) => {
+    let path = 'index.html'
+    sendHtml(response, path)
+})
+
+const sendJSON = (response, data) => {
+    let r = JSON.stringify(data, null, 2)
+    response.send(r)
+}
+
+app.get('/todo/all', (requrest, response) => {
+    sendJSON(response, todoList)
+})
+
+const todoAdd = (form) => {
+    if (todoList.length === 0) {
+        form.id = 1
+    } else {
+        let lastTodo = todoList[todoList.length - 1]
+        form.id = lastTodo.id + 1
+    }
+    form.done = false
+    todoList.push(form)
+    return form
+}
+
+
+app.post('/todo/add', (request, response) => {
+    let form = request.body
+    let todo = todoAdd(form)
+    sendJSON(response, todo)
+})
+
+const todoDelete = (id) => {
+    id = Number(id)
+    let index = -1
+    for (let i = 0; i < todoList.length; i++) {
+        let t = todoList[i]
+        if (t.id === id) {
+            // 找到了
+            index = i
+            break
+        }
+    }
+    if (index > -1) {
+        // 找到了, 用 splice 函数来删除
+        // splice 函数返回的是包含被删除元素的数组
+        // 所以要用 [0] 取出数据
+        let t = todoList.splice(index, 1)[0]
+        return t
+    } else {
+        // 没找到
+        return {}
+    }
+}
+
+
+app.get('/todo/delete/:id', (request, response) => {
+    let id = request.params.id
+    let todo = todoDelete(id)
+    sendJSON(response, todo)
+})
+
+const todoCompleted = (id) => {
+    id = Number(id)
+    let index = -1
+    for (let i = 0; i < todoList.length; i++) {
+        let t = todoList[i]
+        if (t.id === id) {
+            // 找到了
+            index = i
+            break
+        }
+    }
+    if (index > -1) {
+        // 找到了, 用 splice 函数来删除
+        // splice 函数返回的是包含被删除元素的数组
+        // 所以要用 [0] 取出数据
+        todoList[index].done = !todoList[index].done
+        let t = todoList[index]
+        return t
+    } else {
+        // 没找到
+        return {}
+    }
+}
+
+app.get('/todo/completed/:id', (request, response) => {
+    let id = request.params.id
+    let todo = todoCompleted(id)
+    sendJSON(response, todo)
+})
+
+
+const main = () => {
+    let server = app.listen(5000, '127.0.0.1', () => {
+        let host = server.address().address
+        let port = server.address().port
+
+        log(`应用实例，访问地址为 http://${host}:${port}`)
+    })
+}
+
+
+if (require.main === module) {
+    main()
+}
---
+++ todo_span/static_files/js/main.js
@@ -0,0 +1,170 @@
+const log = console.log.bind(console)
+
+const ajax = (method, path, data, callback) => {
+    let p = new Promise((resolve, reject) => {
+        let r = new XMLHttpRequest()
+        r.open(method, path, true)
+        r.setRequestHeader('Content-Type', 'application/json')
+        r.onreadystatechange = () => {
+            if (r.readyState === 4) {
+                resolve(r.response)
+            }
+        }
+        if (method === 'POST') {
+            data = JSON.stringify(data)
+        }
+        r.send(data)
+    })
+    return p
+}
+
+const apiTodoAll = (callback) => {
+    let method = 'GET'
+    let path = '/todo/all'
+    let data = ''
+    return ajax(method, path, data, callback)
+}
+
+const apiTodoAdd = (task, callback) => {
+    let method = 'POST'
+    let path = '/todo/add'
+    let data = {
+        task: task,
+    }
+    return ajax(method, path, data, callback)
+}
+
+const apiTodoDelete = (id, callback) => {
+    let method = 'GET'
+    let path = '/todo/delete/' + String(id)
+    let data = ''
+    return ajax(method, path, data, callback)
+}
+
+const apiTodoCompleted = (id, callback) => {
+    let method = 'GET'
+    let path = '/todo/completed/' + String(id)
+    let data = ''
+    return ajax(method, path, data, callback)
+}
+
+const e = (sel) => {
+    return document.querySelector(sel)
+}
+
+const bindEvent = (element, eventName, callback) => {
+    element.addEventListener(eventName, callback)
+}
+
+const appendHtml = (element, html) => {
+    element.insertAdjacentHTML('beforeend', html)
+}
+
+const templateTodo = (todo) => {
+    let {id, task, done} = todo
+    let d1 = done ?  'fa-check-circle' : 'fa-circle-thin'
+    let d2 = done ? 'lineThrough' : ''
+
+    let t = `
+         <li class="item yy-cell ">
+                     <i class="fa  co yy-todo-completed ${d1}"  id="${id}"></i>
+                     <p class="text ${d2}">${task}</p>
+                     <i class="fa fa-trash-o de yy-todo-delete " id="${id}"></i>  
+            </li>
+    `
+    return t
+}
+
+const insertTodo = (todo) => {
+    let container = e('.yy-todo-container')
+    let t = templateTodo(todo)
+    appendHtml(container, t)
+}
+
+const loadTodos = (todos) => {
+    e('.yy-todo-container').innerHTML = ''
+    let ts = JSON.parse(todos)
+    for (let t of ts) {
+        insertTodo(t)
+    }
+}
+
+class Action {
+    constructor() {
+
+    }
+
+    static new(...args) {
+        return new this(...args)
+    }
+
+    add(self) {
+
+        let task = e('.yy-input').value
+        apiTodoAdd(task)
+    }
+
+    delete(self) {
+        let id = self.id
+        log('id', id)
+        apiTodoDelete(id)
+    }
+
+    completed(self) {
+        let id = self.id
+        log('id', id)
+        apiTodoCompleted(id)
+    }
+}
+
+const bindEventTodo = () => {
+    bindEvent(e('body'), 'click', event => {
+        let self = event.target
+        log('self', self)
+        let apiAction = Action.new()
+        let has = self.classList.contains.bind(self.classList)
+
+        if (has('yy-todo-add')) {
+            apiAction.add(self)
+        }
+        if (has('yy-todo-delete')) {
+            apiAction.delete(self)
+        }
+        if (has('yy-todo-completed')) {
+            apiAction.completed(self)
+        }
+
+        loadTodoAll()
+    })
+}
+
+const bindEvents = () => {
+    bindEventTodo()
+}
+
+const loadTodoAll = async () => {
+    // 用同步的思维写异步程序
+    let l2 = await apiTodoAll()
+    log('l2', l2)
+    // let d1 = await apiTodoDelete(2)
+    loadTodos(l2)
+}
+
+const initDate = () => {
+    let options = {weekday : "long", month:"short", day:"numeric"}
+    let today = new Date()
+
+    e('#date').innerHTML = today.toLocaleDateString("en-US", options)
+}
+
+const init = () => {
+    initDate()
+    loadTodoAll()
+}
+
+const __main = () => {
+    init()
+    bindEvents()
+}
+
+__main()
