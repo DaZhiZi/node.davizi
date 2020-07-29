455b2aa315a96d650ec46cc8753026b7e9f32ed7
大侄子 <55380215+DaZhiZi@users.noreply.github.com>
2020-07-29 08:30:34 +0800

    Update blog.js

--- blog/model/blog.js
+++ blog/model/blog.js
@@ -2,11 +2,8 @@ const fs = require('fs')
 
 const blogFilePath = 'db/blog.json'
 
-// 这是一个用来存储 Blog 数据的对象
 class ModelBlog {
     constructor(form) {
-        // || 是一种新的写法，在 js 圈子里太过流行，所以记住即可
-        // a = b || c 意思是如果 b 会转成 false（比如 undefined, null），就把 c 赋值给 a
         this.title = form.title || ''
         this.author = form.author || ''
         this.content = form.content || ''
@@ -16,28 +13,17 @@ class ModelBlog {
 }
 
 const loadBlogs = () => {
-    // 确保文件有内容，这里就不用处理文件不存在或者内容错误的情况了
     let content = fs.readFileSync(blogFilePath, 'utf8')
     let blogs = JSON.parse(content)
     // console.log('load blogs', blogs)
     return blogs
 }
-
-/*
-b 这个对象是我们要导出给别的代码用的对象
-它有一个 data 属性用来存储所有的 blogs 对象
-它有 all 方法返回一个包含所有 blog 的数组
-它有 new 方法来在数据中插入一个新的 blog 并且返回
-它有 save 方法来保存更改到文件中
-*/
-
 const b = {
     data: loadBlogs(),
 }
 
 b.all = function() {
     let blogs = this.data
-    // 遍历 blog，插入 comments
     let comment = require('./comment')
     let comments = comment.all()
     for (let i = 0; i < blogs.length; i++) {
@@ -72,10 +58,6 @@ b.new = function(form) {
     return m
 }
 
-/*
-它能够删除指定 id 的数据
-删除后保存修改到文件中
-*/
 b.delete = function(id) {
     let blogs = this.data
     let found = false
@@ -86,11 +68,7 @@ b.delete = function(id) {
             break
         }
     }
-    // 用 splice 函数删除数组中的一个元素
-    // 如果没有找到，i 的值就是无用值，删除也不会报错
-    // 所以不用判断也可以
     blogs.splice(i, 1)
-    // 不返回数据也行，但是还是返回一下好了
     return found
 }
 
@@ -105,6 +83,4 @@ b.save = function() {
     })
 }
 
-// 导出一个对象的时候用 module.exports = 对象 的方式
-// 这样引用的时候就可以直接把模块当这个对象来用了（具体看使用方法）
 module.exports = b
